1d2cdfbb7694088fe8cb5c333bf6c08375e2475e
Abhi987527 <149317871+Abhi987527@users.noreply.github.com>
2023-10-29 21:06:09 +0530

    Add files via upload

---
+++ website.php
@@ -0,0 +1,192 @@
+<?php
+<!DOCTYPE html>
+
+<html>
+<head>
+
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9826687827402813"
+     crossorigin="anonymous"></script>
+     <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-WFBXHQF697"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-WFBXHQF697');
+</script>
+
+
+
+        <meta charset="utf-8"/>
+		<title>HonorsMoney - Earn Points, Claim Gift Cards </title>
+		<meta content="width=device-width, initial-scale=1" name="viewport"/>
+		
+		<!-- Description for your website -->
+		<meta name="description" content="Earn Free Gift Cards by Completing offers, Downloading Apps/Games or even Referring your friends. What are you waiting for, Claim Your Free Gift Cards Today!"/>
+		
+		
+		<meta content="Webflow" name="generator"/>
+		<link href="css/the-plug.webflow.5effc961b.css" rel="stylesheet" type="text/css"/>
+		<script src="dashboard/assets/ajax/libs/webfont/1.6.26/webfont.js" type="text/javascript"></script>
+		
+		<!-- Favicon -->
+		<link rel="icon" type="image/ico" href="img/favicon.ico" />
+		
+		<!-- Image for Search Result (Next to your Website) -->
+		<meta property="og:image" src="img/IMAGE.png" />
+		
+	</head>
+	<body class="body">
+	    
+	   
+
+		<nav id="nav" class="sticky-nav">
+			<div class="div-block">
+				<ul class="nav-grid w-list-unstyled">
+					<li id="w-node-ae401f243477-47f866f9">
+						<a href="#" class="nav-logo-link w-inline-block">
+						
+						<!-- TOP Left Logo -->
+							<img src="img/Logo.png" alt="" class="nav-logo"/> 
+							
+						</a>
+					</li>
+					<li class="list-item">
+						<a href="/dashboard" class="nav-link">Dashboard</a>
+					</li>
+					<li id="w-node-ae401f243480-47f866f9" class="list-item-4">
+						<a href="dashboard/login.php" class="button w-button">LOGIN</a>
+					</li>
+					<li id="w-node-2da8d23fba34-47f866f9" class="list-item-5">
+						<a href="dashboard/register.php" class="signup-button w-button">SIGNUP</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+		<div class="section">
+			<div class="div-block">
+				<div class="w-layout-grid grid">
+					<div id="w-node-9a065fb36f44-47f866f9" class="div-block-8">
+						<h5 class="tags">HonorsMoney</h5>
+						<h1 class="heading">Earn Gift Cards,</h1>
+						<h2 class="heading-3">For 
+							<strong>FREE</strong> right now by 
+							<strong class="bold-text-6">playing games</strong> and completing tasks!
+							<br/>
+						</h2>
+						<div class="tagline">What are you waiting for? Sign up now!
+							<br/>
+						</div>
+						<a href="/dashboard" class="hero-button w-button">START EARNING TODAY</a>
+					</div>
+					<div>
+						<img src="img/IMAGE.png" srcset="img/IMAGE.png" sizes="(max-width: 479px) 92vw, (max-width: 767px) 95vw, 96vw" alt="" class="hero-image"/>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="section-3">
+			<div class="div-block">
+				<div class="w-layout-grid grid-3">
+					<div class="feature-div">
+						<h1 class="feature-heading">1. Signup</h1>
+						<p class="feature-text">Register for an account to store and keep track of your earnings.
+							<br/>
+						</p>
+					</div>
+					<div class="feature-div">
+						<h1 class="feature-heading">2. Earn Points</h1>
+						<p class="feature-text">Download phone apps or complete simple tasks to easily earn points.
+							<br/>
+						</p>
+					</div>
+					<div class="feature-div">
+						<h1 class="feature-heading">3. Cashout</h1>
+						<p class="feature-text">Instantly exchange points for any Reward or Gift Card of your choice!
+							<br/>
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="section-2">
+			<div class="div-block">
+				<div class="w-layout-grid grid-2">
+					<div id="w-node-080419dae458-47f866f9" class="div-block-15">
+						<img src="img/IMAGE2.png" srcset="img/IMAGE2.png" sizes="(max-width: 479px) 300px, (max-width: 767px) 450px, (max-width: 991px) 350px, 450px" alt="" class="image"/>
+					</div>
+					<div id="w-node-257e19b80ce5-47f866f9">
+						<div class="div-block-11">
+							<h5 class="tags-white">EARN POINTS</h5>
+							<h1 class="heading">Earning Points is Quick and Easy!</h1>
+							<h2 class="heading-3">Just download one of our many 
+								<strong class="bold-text-2">awesome</strong> sponsored games or apps, play it for a few minutes and 
+								<strong class="bold-text-3">thats it!</strong>
+								<br/>
+							</h2>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="section-4">
+			<div class="div-block">
+				<div class="w-layout-grid grid-6">
+					<div id="w-node-158ca2461b4c-47f866f9" class="div-block-8">
+						<h5 class="tags-copy">REFER AND EARN</h5>
+						<h1 class="heading">Refer friends, earn points!</h1>
+						<h2 class="heading-3">Don&#x27;t want to work? 
+							<strong class="bold-text-4">Refer your friends</strong> and let them earn
+							<strong class="bold-text-5"> Points for you!</strong>
+							<br/>
+						</h2>
+					</div>
+					<div class="div-block-14">
+						<img src="img/IMAGE3.png" srcset="img/IMAGE3.png" sizes="(max-width: 479px) 92vw, (max-width: 767px) 95vw, (max-width: 991px) 96vw, 87vw" alt="" class="image-4"/>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="section-2">
+			<div class="div-block">
+				<div class="w-layout-grid grid-2">
+					<div id="w-node-b54caafcdec6-47f866f9" class="div-block-6">
+						<img src="img/Cards.png" srcset="img/Cards.png" sizes="(max-width: 479px) 73vw, (max-width: 991px) 76vw, 745.609375px" alt="" class="giftcards"/>
+					</div>
+					<div id="w-node-b54caafcdec8-47f866f9" class="div-block-9">
+						<div class="div-block-10">
+							<h5 class="tags-white">CASHOUT</h5>
+							<h1 class="heading">Redeem Your Points for Awesome Rewards</h1>
+							<h2 class="heading-3">Redeem your points for 
+								<strong class="bold-text">Free</strong> Gift Cards!
+								<br/>
+							</h2>
+							<a href="/dashboard" class="hero-button-alt w-button">GRAB YOUR FREE Gift Cards</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="section-5">
+			<div class="div-block">
+				<div class="w-layout-grid footer-grid">
+					<div class="div-block-12">
+					
+					<!-- Bottom Left Logo -->
+						<img src="img/Logo.png" srcset="img/Logo.png" sizes="(max-width: 479px) 55vw, (max-width: 767px) 37vw, 228px" alt="" class="image-3"/>
+					
+					</div>
+					<div id="w-node-60d2480c105c-47f866f9" class="div-block-13">
+						<a href="Terms.html" class="footer-nav-link">Terms and Conditions</a>
+						<a href="Privacy.html" class="footer-nav-link">Privacy Policy</a>
+						<a href="Contact.html" class="footer-nav-link">Contact</a>
+					</div>
+				</div>
+			</div>
+			
+        </div>
+	</body>
+</html>
+
+?>
